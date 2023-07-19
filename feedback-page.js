692f0bf5fce400b202e403c16ee8f000b1d5bcda
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-19 22:28:28 +0200

    Deleted comments

--- feedback-page.js
+++ feedback-page.js
@@ -20,38 +20,3 @@ Stars[0].onclick = () => {
     Stars[0].src = "./assets/starafterclick.svg";
   }
 };
-
-//   star.addEventListener("click", (clickedstar) => {
-//     console.log(clickedstar);
-//     console.log(clickedstar.target);
-//     console.log(clickedstar.currentTarget);
-//     if (clickedstar.target.currentSrc === ".http://127.0.0.1:5500/assets/starafterclick.svg") {
-//       clickedstar.target.currentSrc = "http://127.0.0.1:5500/assets/star.svg";
-//     } else {
-//       for (let k = 0; k < Stars.length; k++) {
-//         Stars[k].src = "./assets/star.svg";
-//       }
-//       for (let j = 0; j <= i; j++) {
-//         Stars[j].src = "./assets/starafterclick.svg";
-//       }
-//     }
-//     console.log(typeof Stars[3].src);
-//     console.log(Stars[3].src === "./assets/starafterclick.svg");
-//   });
-
-// const text = document.getElementsByTagName("input");
-// let keyCounter = 0;
-// text.innertText = "";
-// text[0].addEventListener("keyup", (key) => {
-//   console.log(key);
-//   if (key.code === "Backspace") {
-//     keyCounter--;
-//     text.innertText = text.innertText.slice(0, -1);
-//     console.log(text.innertText);
-//   } else {
-//     if (key.shiftKey === true) {
-//       text.innertText += "";
-//     }
-//   }
-//   console.log(keyCounter);
-// });
