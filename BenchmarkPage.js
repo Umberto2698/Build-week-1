4abba1227639452c838d6488baf1fb82e0c71591
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-19 15:13:25 +0200

    Update BenchmarkPage.js

--- BenchmarkPage.js
+++ BenchmarkPage.js
@@ -261,30 +261,6 @@ function formatTime(time) {
 }
 
 //Funzionalità quiz game
-
-// Options for the observer (which mutations to observe)
-// const config = { innerText: 0 };
-
-// // Callback function to execute when mutations are observed
-// const callback = (mutationList, observer) => {
-//   for (const mutation of mutationList) {
-//     if (mutation.type === "childList") {
-//       console.log("A child node has been added or removed.");
-//     } else if (mutation.type === "attributes") {
-//       console.log(`The ${mutation.attributeName} attribute was modified.`);
-//     }
-//   }
-// };
-
-// // Create an observer instance linked to the callback function
-// const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-//observer.observe(targetNode, config);
-
-// Later, you can stop observing
-//observer.disconnect();
-
 let indiciIncorrectAnswers = [];
 let randomIndex = () => {
   for (let i = 0; i < 3; i++) {
