6054228e5bb6728d01ff6280e45d6126e99156b5
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-19 11:42:06 +0200

    United JavaScript pages

--- BenchmarkPage.js
+++ BenchmarkPage.js
@@ -81,6 +81,38 @@ const questions = [
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
 ];
+const timeLimit = 30;
+let timePassed = 0;
+let timeLeft = timeLimit;
+let timerInterval = null;
+
+const div = document.getElementsByClassName("timer")[0];
+const p = document.getElementsByTagName("p")[0];
+p.innerHTML = `second <br /> <span class="spanTimer">${timeLimit}</span> <br /> remaning`;
+div.appendChild(p);
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = timeLimit - timePassed;
+    p.innerHTML = `second <br /> <span class="spanTimer">${timeLeft}</span> <br /> remaning`;
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+function formatTime(time) {
+  let seconds = time % 60;
+  return `${seconds}`;
+}
+
 const body = document.getElementsByTagName("body")[0];
 const main = document.getElementsByTagName("main")[0];
 const question = document.createElement("div");
