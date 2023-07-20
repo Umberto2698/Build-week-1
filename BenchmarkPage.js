bd6eb0212c0a5a3f008a8318a472b59e5b9f7887
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-20 17:01:03 +0200

    Corrected question title

--- BenchmarkPage.js
+++ BenchmarkPage.js
@@ -11,8 +11,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    question: `In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?`,
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
   },
