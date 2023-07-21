a6731e29a238c2c9c05b845be4202eba7e257a83
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-21 08:54:10 +0200

    Added new questions based on difficulty

--- BenchmarkPage.js
+++ BenchmarkPage.js
@@ -1,4 +1,171 @@
-const questions = [
+const allQuestions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "Which of the following is the oldest of these computers by release date?",
+    correct_answer: "TRS-80",
+    incorrect_answers: ["Commodore 64", "ZX Spectrum", "Apple 3"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "What internet protocol was documented in RFC 1459?",
+    correct_answer: "IRC",
+    incorrect_answers: ["HTTP", "HTTPS", "FTP"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "What was the first company to use the term &quot;Golden Master&quot;?",
+    correct_answer: "Apple",
+    incorrect_answers: ["IBM", "Microsoft", "Google"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question:
+      "Released in 2001, the first edition of Apple&#039;s Mac OS X operating system (version 10.0) was given what animal code name?",
+    correct_answer: "Cheetah",
+    incorrect_answers: ["Puma", "Tiger", "Leopard"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "Dutch computer scientist Mark Overmars is known for creating which game development engine?",
+    correct_answer: "Game Maker",
+    incorrect_answers: ["Stencyl", "Construct", "Torque 2D"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "Who invented the &quot;Spanning Tree Protocol&quot;?",
+    correct_answer: "Radia Perlman",
+    incorrect_answers: ["Paul Vixie", "Vint Cerf", "Michael Roberts"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "Which of the following computer components can be built using only NAND gates?",
+    correct_answer: "ALU",
+    incorrect_answers: ["CPU", "RAM", "Register"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "What is the codename of the eighth generation Intel Core micro-architecture launched in October 2017?",
+    correct_answer: "Coffee Lake",
+    incorrect_answers: ["Sandy Bridge", "Skylake", "Broadwell"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question:
+      "Lenovo acquired IBM&#039;s personal computer division, including the ThinkPad line of laptops and tablets, in what year?",
+    correct_answer: "2005",
+    incorrect_answers: ["1999", "2002", "2008"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "Who is the founder of Palantir?",
+    correct_answer: "Peter Thiel",
+    incorrect_answers: ["Mark Zuckerberg", "Marc Benioff", "Jack Dorsey"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Whistler was the codename of this Microsoft Operating System.",
+    correct_answer: "Windows XP",
+    incorrect_answers: ["Windows 2000", "Windows 7", "Windows 95"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question: "On which computer hardware device is the BIOS chip located?",
+    correct_answer: "Motherboard",
+    incorrect_answers: ["Hard Disk Drive", "Central Processing Unit", "Graphics Processing Unit"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Nvidia&#039;s headquarters are based in which Silicon Valley city?",
+    correct_answer: "Santa Clara",
+    incorrect_answers: ["Palo Alto", "Cupertino", "Mountain View"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question: "In the server hosting industry IaaS stands for...",
+    correct_answer: "Infrastructure as a Service",
+    incorrect_answers: ["Internet as a Service", "Internet and a Server", "Infrastructure as a Server"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "In the programming language &quot;Python&quot;, which of these statements would display the string &quot;Hello World&quot; correctly?",
+    correct_answer: "print(&quot;Hello World&quot;)",
+    incorrect_answers: [
+      "console.log(&quot;Hello World&quot;)",
+      "echo &quot;Hello World&quot;",
+      "printf(&quot;Hello World&quot;)",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question: "In computing terms, typically what does CLI stand for?",
+    correct_answer: "Command Line Interface",
+    incorrect_answers: ["Common Language Input", "Control Line Interface", "Common Language Interface"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which of the following is a personal computer made by the Japanese company Fujitsu?",
+    correct_answer: "FM-7",
+    incorrect_answers: ["PC-9801", "Xmillennium ", "MSX"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "medium",
+    question: "It&#039;s not possible to format a write-protected DVD-R Hard Disk.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "medium",
+    question: "Android versions are named in alphabetical order.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which programming language was developed by Sun Microsystems in 1995?",
+    correct_answer: "Java",
+    incorrect_answers: ["Python", "Solaris OS", "C++"],
+  },
   {
     category: "Science: Computers",
     type: "multiple",
@@ -80,6 +247,13 @@ const questions = [
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
 ];
+
+let questions = [];
+for (let i = 0; i < allQuestions.length; i++) {
+  if (allQuestions[i].difficulty === "easy") {
+    questions.push(allQuestions[i]);
+  }
+}
 
 const header = document.createElement("header");
 const body = document.getElementsByTagName("body")[0];
@@ -293,7 +467,7 @@ const displayAnswer = () => {
         }
       }
       incorrectAnswer = questions.length - correctAnswer;
-      let correctGradient = (correctAnswer * 360) / 10;
+      let correctGradient = (correctAnswer * 360) / questions.length;
       corrPercentage();
       incorrPercentage();
       chartText();
@@ -322,10 +496,7 @@ const displayAnswer = () => {
         </footer>
         </main>
         <script src="./BenchmarkPage.js"></script>`;
-    } else if (
-      questions[questionNumber].correct_answer === "False" ||
-      questions[questionNumber].correct_answer === "True"
-    ) {
+    } else if (questions[questionNumber].type === "boolean") {
       main.innerHTML = "";
       question.innerHTML = "";
       const h1 = document.createElement("h1");
