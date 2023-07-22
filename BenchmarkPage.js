e681363efd6555010a44f372011a9b855b038adb
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-22 15:31:26 +0200

    Added Questions database

--- BenchmarkPage.js
+++ BenchmarkPage.js
@@ -1,246 +1,3 @@
-const allQuestions = [
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: "Which of the following is the oldest of these computers by release date?",
-    correct_answer: "TRS-80",
-    incorrect_answers: ["Commodore 64", "ZX Spectrum", "Apple 3"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: "What internet protocol was documented in RFC 1459?",
-    correct_answer: "IRC",
-    incorrect_answers: ["HTTP", "HTTPS", "FTP"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: `What was the first company to use the term "Golden Master"?`,
-    correct_answer: "Apple",
-    incorrect_answers: ["IBM", "Microsoft", "Google"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: `Released in 2001, the first edition of Apple's Mac OS X operating system (version 10.0) was given what animal code name?`,
-    correct_answer: "Cheetah",
-    incorrect_answers: ["Puma", "Tiger", "Leopard"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: "Dutch computer scientist Mark Overmars is known for creating which game development engine?",
-    correct_answer: "Game Maker",
-    incorrect_answers: ["Stencyl", "Construct", "Torque 2D"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: `Who invented the "Spanning Tree Protocol"?`,
-    correct_answer: "Radia Perlman",
-    incorrect_answers: ["Paul Vixie", "Vint Cerf", "Michael Roberts"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: "Which of the following computer components can be built using only NAND gates?",
-    correct_answer: "ALU",
-    incorrect_answers: ["CPU", "RAM", "Register"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: "What is the codename of the eighth generation Intel Core micro-architecture launched in October 2017?",
-    correct_answer: "Coffee Lake",
-    incorrect_answers: ["Sandy Bridge", "Skylake", "Broadwell"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: `Lenovo acquired IBM's personal computer division, including the ThinkPad line of laptops and tablets, in what year?`,
-    correct_answer: "2005",
-    incorrect_answers: ["1999", "2002", "2008"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "hard",
-    question: "Who is the founder of Palantir?",
-    correct_answer: "Peter Thiel",
-    incorrect_answers: ["Mark Zuckerberg", "Marc Benioff", "Jack Dorsey"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "medium",
-    question: "Whistler was the codename of this Microsoft Operating System.",
-    correct_answer: "Windows XP",
-    incorrect_answers: ["Windows 2000", "Windows 7", "Windows 95"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "medium",
-    question: "On which computer hardware device is the BIOS chip located?",
-    correct_answer: "Motherboard",
-    incorrect_answers: ["Hard Disk Drive", "Central Processing Unit", "Graphics Processing Unit"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "medium",
-    question: `Nvidia's headquarters are based in which Silicon Valley city?`,
-    correct_answer: "Santa Clara",
-    incorrect_answers: ["Palo Alto", "Cupertino", "Mountain View"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "medium",
-    question: "In the server hosting industry IaaS stands for...",
-    correct_answer: "Infrastructure as a Service",
-    incorrect_answers: ["Internet as a Service", "Internet and a Server", "Infrastructure as a Server"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "medium",
-    question: `In the programming language "Python", which of these statements would display the string "Hello World" correctly?`,
-    correct_answer: `print("Hello World")`,
-    incorrect_answers: [`console.log("Hello World")`, `echo "Hello World"`, `printf("Hello World")`],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "medium",
-    question: "In computing terms, typically what does CLI stand for?",
-    correct_answer: "Command Line Interface",
-    incorrect_answers: ["Common Language Input", "Control Line Interface", "Common Language Interface"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "medium",
-    question: "Which of the following is a personal computer made by the Japanese company Fujitsu?",
-    correct_answer: "FM-7",
-    incorrect_answers: ["PC-9801", "Xmillennium ", "MSX"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "medium",
-    question: `It's not possible to format a write-protected DVD-R Hard Disk.`,
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "medium",
-    question: "Android versions are named in alphabetical order.",
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "medium",
-    question: "Which programming language was developed by Sun Microsystems in 1995?",
-    correct_answer: "Java",
-    incorrect_answers: ["Python", "Solaris OS", "C++"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What does CPU stand for?",
-    correct_answer: "Central Processing Unit",
-    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: `In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?`,
-    correct_answer: "Final",
-    incorrect_answers: ["Static", "Private", "Public"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "The logo for Snapchat is a Bell.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What is the most preferred image format used for logos in the Wikimedia database?",
-    correct_answer: ".svg",
-    incorrect_answers: [".png", ".jpeg", ".gif"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "In web design, what does CSS stand for?",
-    correct_answer: "Cascading Style Sheet",
-    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What is the code name for the mobile operating system Android 7.0?",
-    correct_answer: "Nougat",
-    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "On Twitter, what is the character limit for a Tweet?",
-    correct_answer: "140",
-    incorrect_answers: ["120", "160", "100"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Linux was first created as an alternative to Windows XP.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "Which programming language shares its name with an island in Indonesia?",
-    correct_answer: "Java",
-    incorrect_answers: ["Python", "C", "Jakarta"],
-  },
-];
-
 let questions = [];
 let timePassed = 0;
 let timeGradient = 0;
@@ -302,13 +59,13 @@ let lastClickedText = [];
 let last = "";
 
 function corrPercentage(correctAnswer) {
-  correctH4.innerText = `${(correctAnswer * 100) / questions.length}%`;
-  correctH5.innerText = `${correctAnswer} / ${questions.length} questions`;
+  correctH4.innerText = `${(correctAnswer * 100) / 10}%`;
+  correctH5.innerText = `${correctAnswer} / ${10} questions`;
 }
 
 function incorrPercentage(correctAnswer, incorrectAnswer) {
-  incorrectH4.innerText = `${100 - (correctAnswer * 100) / questions.length}%`;
-  incorrectH5.innerText = `${incorrectAnswer} / ${questions.length} questions`;
+  incorrectH4.innerText = `${100 - (correctAnswer * 100) / 10}%`;
+  incorrectH5.innerText = `${incorrectAnswer} / ${10} questions`;
 }
 
 function chartText(correctAnswer) {
@@ -361,7 +118,7 @@ function formatTime(time) {
 let indexRandomQuestions = [];
 let randomQuestions = () => {
   for (let i = 0; i < 10; i++) {
-    let indice = parseInt(Math.floor(Math.random() * 10));
+    let indice = parseInt(Math.floor(Math.random() * 30));
     if (indexRandomQuestions.some((n) => n === indice)) {
       i--;
       continue;
@@ -477,18 +234,18 @@ const displayAnswer = () => {
   buttons = [];
   const nextQuestion = setTimeout(function () {
     questionNumber++;
-    if (questionNumber === questions.length) {
+    if (questionNumber === 10) {
       clearInterval(setInterval);
       let correctQuestionsAnswer = [];
       let correctAnswer = 0;
-      for (let i = 0; i < questions.length; i++) {
+      for (let i = 0; i < 10; i++) {
         correctQuestionsAnswer.push(questions[indexRandomQuestions[i]].correct_answer);
         if (correctQuestionsAnswer[i] === lastClickedText[i]) {
           correctAnswer += 1;
         }
       }
-      let incorrectAnswer = questions.length - correctAnswer;
-      let correctGradient = (correctAnswer * 360) / questions.length;
+      let incorrectAnswer = 10 - correctAnswer;
+      let correctGradient = 360 - (correctAnswer * 360) / 10;
       corrPercentage(correctAnswer);
       incorrPercentage(correctAnswer, incorrectAnswer);
       chartText(correctAnswer);
@@ -501,7 +258,7 @@ const displayAnswer = () => {
         <h4>${correctH4.innerText}</h4>
         <h5>${correctH5.innerText}</h5>
         </div>
-        <div style="background-image: conic-gradient(#00ffff ${correctGradient}deg, #c2128d ${correctGradient}deg); box-shadow: 0px 0px 20px 10px #090D2C;">
+        <div style="background-image: conic-gradient(#c2128d ${correctGradient}deg, #00ffff ${correctGradient}deg); box-shadow: 0px 0px 20px 10px #090D2C;">
         <div id="chart">
         <p>${pText.innerHTML}</p>
         <p>${secondP.innerHTML}</p>
@@ -534,7 +291,6 @@ const displayAnswer = () => {
       </div>`;
       startTimer(timeLimit, timePassed);
       let position = randomQuestions();
-      console.log(indexRandomQuestions);
       if (questions[position].type === "boolean") {
         main.innerHTML = "";
         question.innerHTML = "";
